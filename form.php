ed39126ee8953a60c586aac77b700257263fcc97
Kridak <48737384+Kridak@users.noreply.github.com>
2019-09-18 21:05:04 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,28 @@
+<form method="post" action="form_cs.php">
+	<table align="center">
+		<tr>
+			<td>Registration</td>
+			<td><input type="text" name="t1" value=""></td>
+		</tr>
+		<tr>
+			<td>Student Name</td>
+			<td><input type="text" name="t2" value=""></td>
+		</tr>
+		<tr>
+			<td>Faculty Name</td>
+			<td><input type="text" name="t3" value=""></td>
+		</tr>
+		<tr>
+			<td>SDA</td>
+			<td><input type="text" name="t4" value=""></td>
+		</tr>
+		<tr>
+			<td>Activity</td>
+			<td><input type="text" name="t5" value=""></td>
+		</tr>
+		
+		<tr>
+			<td colspan="2" align="center"><input type="submit" name="sub" value="Send"></td>
+		</tr>
+	</table>
+</form>
