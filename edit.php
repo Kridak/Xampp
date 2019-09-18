2692635e329aec990a1299c0609d8418df49d821
Kridak <48737384+Kridak@users.noreply.github.com>
2019-09-18 21:20:45 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,30 +1,29 @@
 <?php
 include "connection.php";
 $id=$_REQUEST['uid'];
-$sql="select * from user where id='$id'";
+$sql="select * from oo where rn='$rn'";
 $res=mysqli_query($con,$sql);
 $arr=mysqli_fetch_array($res);
 ?>
 <form method="post" action="edit_cs.php">
-<input type="hidden" name="uid" value="<?php echo $arr['id']; ?>">
+<input type="hidden" name="uid" value="<?php echo $arr['rn']; ?>">
 <table align="center">
 <tr>
-<td>Name</td>
-<td><input type="text" name="t1" value="<?php echo $arr['name']; ?>"></td>
+<td>Student Name</td>
+<td><input type="text" name="t1" value="<?php echo $arr['st']; ?>"></td>
 </tr>
 <tr>
-<td>Email Id</td>
-<td><input type="text" name="t2" value="<?php echo $arr['email']; ?>"></td>
+<td>Faculty Name</td>
+<td><input type="text" name="t2" value="<?php echo $arr['fn']; ?>"></td>
 </tr>
 <tr>
-<td>Mobile No</td>
-<td><input type="text" name="t3" value="<?php echo $arr['mobile']; ?>"></td>
+<td>SDA Marks</td>
+<td><input type="text" name="t3" value="<?php echo $arr['sd']; ?>"></td>
 </tr>
 <tr>
-<td>Address</td>
-<td><textarea name="txt"><?php echo $arr['address']; ?></textarea></td>
+<td>Activity</td>
+<td><input type="text" name="t3" value="<?php echo $arr['ac']; ?>"></td>
 </tr>
-<tr>
 
 <td colspan="2" align="center"><input type="submit" name="sub" value="Update"></td>
 </tr>
